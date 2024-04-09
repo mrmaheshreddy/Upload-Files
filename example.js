4c769bf244927c07b9dbdfda66eea14b2ea7f465
mahesh_reddy <119918525+mrmaheshreddy@users.noreply.github.com>
2024-04-09 16:25:43 +0530

    Add files via upload

---
+++ example.js
@@ -0,0 +1,15 @@
+// let n=5;
+// for(let i=0;i<n;i++){
+//     console.log(i,"hello mahesh",i);
+// }
+
+// console.log(process.argv)
+
+//  let ans = require("./math")
+// console.log(ans.sum(2,9))
+// console.log(ans.PI)
+// console.log(ans.sub(5,2))
+
+
+let ans = require("./FRUITS")
+console.log(ans)
